8b02608c67482ba6ce8f216a7bf4df9948767293
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-10-22 20:05:04 +0700

    Add files via upload

--- configwriter_systemd.py
+++ configwriter_systemd.py
@@ -1,36 +1,33 @@
-import os 
-import getpass 
+import os
+import pwd
+import time
 import configparser 
-user = getpass.getuser()
+
+user =  pwd.getpwuid(os.getuid())[0]
 Generate_path = "/usr/lib/systemd/system/" 
+os.system("sudo chmod -R 777 "+Generate_path)
 project_name = 'Project:RoboreactorGenFlow'
 mode = 'multi-user.target' 
 type = 'idle'
 wanted = 'WantedBy'
 execdat = 'ExecStart'
 Python_exc_path = "/usr/bin/python3 "
-Execute_path = "/home/"+user+"/RoboreactorGenFlow/RoboreactorGenFlow.py"
+Execute_path = "/home/"+'pi'+"/RoboreactorGenFlow/RoboreactorGenFlow.py"
 config = configparser.ConfigParser() 
+config.optionxform = str
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 settings = ['Unit','Service','Install']
 #Unit
 config.add_section(settings[0]) # Getting the section added into the list topic 
-config.set(settings[0],str('Description'),str(project_name)) 
-config.set(settings[0],str('After'),str(mode))
+config.set(settings[0],'Description',str(project_name)) 
+config.set(settings[0],'After',str(mode))
 #Service 
 config.add_section(settings[1])
-config.set(settings[1],str('type'),str(type))
-config.set(settings[1],str(execdat),str(Python_exc_path+Execute_path))
+config.set(settings[1],'Type',str(type))
+config.set(settings[1],'ExecStart',str(Python_exc_path+Execute_path))
 #Install 
 config.add_section(settings[2])
-config.set(settings[2],str(wanted),str(mode))
+config.set(settings[2],'WantedBy',str(mode))
 configfile = open(Generate_path+"RoboreactorGenFlow.service",'w')
 config.write(configfile)
-#Enable and start service 
-os.system("sudo systemctl daemon-reload")
-os.system("sudo systemctl enable RoboreactorGenFlow.service")
-os.system("sudo systemctl start RoboreactorGenFlow.service")
-os.system("sudo systemctl status RoboreacgorGenFlow.service")
-
-
 
