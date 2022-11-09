1c54e7873184c187978b418734f9e15f6d060400
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-11-09 22:27:49 +0700

    Add files via upload

--- configwriter_systemd.py
+++ configwriter_systemd.py
@@ -4,11 +4,16 @@ import time
 import configparser 
 
 user =  os.listdir("/home/")[0]
+try:
+   os.mkdir("/usr/lib/systemd/system",mode=0o777)
+except:
+    print("System directory was created")
 Generate_path = "/usr/lib/systemd/system/" 
 os.system("sudo chmod -R 777 "+Generate_path)
 project_name = 'Project:RoboreactorGenFlow'
 mode = 'multi-user.target' 
 Python_exc_path = "/usr/bin/python3 "
+Working_path = "/home/"+user+"/RoboreactorGenFlow"
 Execute_path = "/home/"+user+"/RoboreactorGenFlow/RoboreactorGenFlow.py"   #Change username over the platform 
 config = configparser.ConfigParser() 
 config.optionxform = str
@@ -21,7 +26,10 @@ config.set(settings[0],'After',str(mode))
 #Service 
 config.add_section(settings[1])
 config.set(settings[1],'Type',str(type))
+config.set(settings[1],'WorkingDirectory',Working_path)
+config.set(settings[1],'User',str(user))
 config.set(settings[1],'ExecStart',str(Python_exc_path+Execute_path))
+config.set(settings[1],'WantedBy','always')
 #Install 
 config.add_section(settings[2])
 config.set(settings[2],'WantedBy',str(mode))
